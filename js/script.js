dbaa1c632e9f5c50c1a2d68dcf7f64e5aa64db9e
Rafael <rcnelli@hotmail.com>
2023-05-30 16:40:17 -0300

    pagina 2 inserida

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function calcular() {
 
     document.getElementById("avisoc").innerHTML = null;
     document.getElementById("aviso1").innerHTML = null;
-     document.getElementById("aviso2").innerHTML = null;
+    document.getElementById("aviso2").innerHTML = null;
     document.getElementById("aviso1").classList.remove("avisocor2");
     document.getElementById("aviso2").classList.remove("avisocor2");
     document.getElementById("avisoc").classList.remove("avisocor1");
@@ -47,9 +47,49 @@ function calcular() {
 }
 
 
-function media(){
-let n1 = document.getElementById('nota1').value;
-let n2 = document.getElementById('nota2').value;
-let n3 = document.getElementById('nota3').value;
-let media = (n1 + n2 + n3)/3;
+function media() {
+    let n1 = parseFloat(document.getElementById('nota1').value);
+    let n2 = parseFloat(document.getElementById('nota2').value);
+    let n3 = parseFloat(document.getElementById('nota3').value);
+    let med = (n1 + n2 + n3) / 3;
+    return med;
 }
+
+
+function frequencia() {
+    let f1 = parseFloat(document.querySelector('input[name="dia1"]:checked').value);
+    let f2 = parseFloat(document.querySelector('input[name="dia2"]:checked').value);
+    let f3 = parseFloat(document.querySelector('input[name="dia3"]:checked').value);
+    let f4 = parseFloat(document.querySelector('input[name="dia4"]:checked').value);
+    let f5 = parseFloat(document.querySelector('input[name="dia5"]:checked').value);
+    let f6 = parseFloat(document.querySelector('input[name="dia6"]:checked').value);
+    let f7 = parseFloat(document.querySelector('input[name="dia7"]:checked').value);
+    let f8 = parseFloat(document.querySelector('input[name="dia8"]:checked').value);
+    let f9 = parseFloat(document.querySelector('input[name="dia9"]:checked').value);
+    let f10 = parseFloat(document.querySelector('input[name="dia10"]:checked').value);
+    let f11 = parseFloat(document.querySelector('input[name="dia11"]:checked').value);
+    let f12 = parseFloat(document.querySelector('input[name="dia12"]:checked').value);
+    let freq = (f1 + f2 + f3 + f4 + f5 + f6 + f7 + f8 + f9 + f10 + f11 + f12) / 12;
+    console.log(freq);
+
+    return freq;
+
+}
+
+
+function VerificaAprovacao() {
+    let m = media();
+    let f = frequencia();
+
+    if (m >= 6 && f >= 0.75) {
+        document.getElementById("result").innerHTML = "Aprovado";
+        document.getElementById("result").classList.add("aprov");
+
+    } else {
+        document.getElementById("result").innerHTML = "Reprovado";
+        document.getElementById("result").classList.add("reprov");
+
+
+    }
+}
+
